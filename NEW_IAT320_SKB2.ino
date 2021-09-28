02f21e5a3988ac48784dab1168ec54c2484b0043
melhsu <90661739+melhsu@users.noreply.github.com>
2021-09-28 12:20:45 +0800

    Add files via upload

---
+++ NEW_IAT320_SKB2.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 8 
+
+Adafruit_NeoPixel led = Adafruit_NeoPixel(3, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  led.begin();
+  led.show(); 
+  led.setBrightness(255);  // Set this to max brightness, so the name can be more viewable
+}
+
+void loop() {
+  led.show();
+  movement();
+  movement();
+}
+
+void movement() {
+  for (int i; i < 200 ; i++) {
+    for(int j=0; j<led.numPixels(); j++) {
+      led.setPixelColor(j, led.Color(i+30,0,0));  // red
+    }
+    led.show();
+    delay(5);
+  }
+  for (int i = 200; i > 0; i--) {
+    for(int j=0; j<led.numPixels(); j++) {
+      led.setPixelColor(j, led.Color(i+30,0,0)); //red
+    }
+    led.show();
+    delay(5);
+  }
+}
